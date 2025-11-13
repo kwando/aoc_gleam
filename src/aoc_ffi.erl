d551e450da2c18cfe3f7e160e62b4e0dc60a5381
Hannes Nevalainen <hannes.nevalainen@me.com>
2025-11-13 06:43:51 +0100

    rename project to just aoc

--- src/aoc_ffi.erl
+++ src/aoc_ffi.erl
@@ -1,28 +1,34 @@
--module(aoc_2024_ffi).
+-module(aoc_ffi).
+
 -export([binary_at/2, new_ets_map/2, ets_map_lookup/2, pd_get/1, pt_get/1]).
 
 binary_at(Bin, Index) ->
     try
         {ok, binary:at(Bin, Index)}
     catch
-        error:badarg -> {error, nil}
+        error:badarg ->
+            {error, nil}
     end.
 
-
 new_ets_map(Bin, Opts) ->
-    ets:new(erlang:binary_to_atom(Bin, utf8), Opts).
+    ets:new(
+        erlang:binary_to_atom(Bin, utf8), Opts).
 
 ets_map_lookup(Table, Key) ->
     ets:lookup_element(Table, Key, 2).
 
 pd_get(Key) ->
     case erlang:get(Key) of
-        undefined -> {error, nil};
-        Value -> {ok, Value}
+        undefined ->
+            {error, nil};
+        Value ->
+            {ok, Value}
     end.
+
 pt_get(Key) ->
     try
         {ok, persistent_term:get(Key)}
     catch
-        _ -> {error, nil}
+        _ ->
+            {error, nil}
     end.
